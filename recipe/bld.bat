04b75926d81fa2267293c898387fb540d0b2361d
PatrykKups <pkups@anaconda.com>
2025-06-25 12:12:25 +0200

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,3 @@
-@echo off
-dir /s
-
-chcp 65001
-if errorlevel 1 exit 1
-
-dir /s
-
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
@@ -16,12 +8,15 @@ cmake -GNinja ^
       -DBUILD_SHARED_LIBS=ON ^
       -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
       -DBUILD_TESTING=ON ^
-      -DCMAKE_CXX_FLAGS="/utf-8" ^
-      -DCMAKE_C_FLAGS="/utf-8" ^
       ..
 if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+REM Encoding issue, setting UTF-8 doesn't help, "Å Éxample.txt" is correct name in upstream, fix for test:
+REM aws_fopen_non_ascii_read_existing_file_test: static: Failed to open file. path:'Å Éxample.txt'
+RENAME "%SRC_DIR%\tests\resources\├à ├ëxample.txt" "Å Éxample.txt"
 if errorlevel 1 exit 1
 
 ctest --output-on-failure

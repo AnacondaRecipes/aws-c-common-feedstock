efd29099661bb0d1b2ddd360f56a9dbf258e58a6
PatrykKups <pkups@anaconda.com>
2025-06-25 10:38:09 +0200

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
+REM set UTF-8 mode by default
+chcp 65001
+
 cmake -GNinja ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
@@ -14,8 +17,5 @@ if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
-@REM aws_fopen_non_ascii_read_existing_file_test: Failed to open file. path:'Ã… Ã‰xample.txt' mode:'r' errno:2 aws-error:44(Unknown Error Code)
-@REM Upstream has file name with special characters 'Å Éxample.txt' https://github.com/awslabs/aws-c-common/blob/main/tests/resources/%C3%85%20%C3%89xample.txt
-@REM Unicode vs UTF-8 encoding issue
-ctest -E "aws_fopen_non_ascii_read_existing_file_test" --output-on-failure
+ctest --output-on-failure
 if errorlevel 1 exit 1
